db4e87e6db16056bcbcd98b3a58201dc4b51ab00
Ondrej Sykora <mail@ondrejsykora.com>
2019-04-15 23:34:45 +0200

    Do not call Application.Quit() in WebGL mode. This does not close the app (there is no way to do that), but it kills the game loop so it looks as if the game crashed.

--- Assets/Scripts/KeyboardController.cs
+++ Assets/Scripts/KeyboardController.cs
@@ -12,7 +12,10 @@ public class KeyboardController : MonoBehaviour
             // When at the main screen (the logo is still visible), kill the app. Otherwise, go to main menu.
             if (GameObject.FindGameObjectWithTag(Tags.Logo))
             {
+#if !UNITY_WEBGL
+                // In WebGL mode, this seems to hang the application.
                 Application.Quit();
+#endif
             }
             else
             {
